be24489621dd8a3792c9282910a1bbdb159cdd0c
Stefan Kersten <kaoskorobase@gmail.com>
2017-11-11 01:57:01 +0100

    Move unsafe_new_stream implementation to header
    Fails tests

--- sodium/sodium.cpp
+++ sodium/sodium.cpp
@@ -14,55 +14,5 @@ namespace sodium {
 
     namespace impl {
 
-        /*!
-         * Creates an stream, that values can be pushed into using impl::send(). 
-         */
-        SODIUM_TUPLE<stream_, SODIUM_SHARED_PTR<node> > unsafe_new_stream()
-        {
-            SODIUM_SHARED_PTR<node> n1(new node);
-            SODIUM_WEAK_PTR<node> n_weak(n1);
-            boost::intrusive_ptr<listen_impl_func<H_STRONG> > impl(
-                new listen_impl_func<H_STRONG>(new listen_impl_func<H_STRONG>::closure([n_weak] (transaction_impl* trans1,
-                        const SODIUM_SHARED_PTR<node>& target,
-                        const SODIUM_SHARED_PTR<holder>& h,
-                        bool suppressEarlierFirings) -> std::function<void()>* {  // Register listener
-                    SODIUM_SHARED_PTR<node> n2 = n_weak.lock();
-                    if (n2) {
-#if !defined(SODIUM_SINGLE_THREADED)
-                        transaction_impl::part->mx.lock();
-#endif
-                        if (n2->link(h.get(), target))
-                            trans1->to_regen = true;
-#if !defined(SODIUM_SINGLE_THREADED)
-                        transaction_impl::part->mx.unlock();
-#endif
-                        if (!suppressEarlierFirings && n2->firings.begin() != n2->firings.end()) {
-                            SODIUM_FORWARD_LIST<light_ptr> firings = n2->firings;
-                            trans1->prioritized(target, [target, h, firings] (transaction_impl* trans2) {
-                                for (SODIUM_FORWARD_LIST<light_ptr>::const_iterator it = firings.begin(); it != firings.end(); it++)
-                                    h->handle(target, trans2, *it);
-                            });
-                        }
-                        SODIUM_SHARED_PTR<holder>* h_keepalive = new SODIUM_SHARED_PTR<holder>(h);
-                        return new std::function<void()>([n_weak, h_keepalive] () {  // Unregister listener
-                            impl::transaction_ trans2;
-                            trans2.impl()->last([n_weak, h_keepalive] () {
-                                std::shared_ptr<node> n3 = n_weak.lock();
-                                if (n3)
-                                    n3->unlink((*h_keepalive).get());
-                                delete h_keepalive;
-                            });
-                        });
-                    }
-                    else
-                        return NULL;
-                }))
-            );
-            n1->listen_impl = boost::intrusive_ptr<listen_impl_func<H_NODE> >(
-                reinterpret_cast<listen_impl_func<H_NODE>*>(impl.get()));
-            boost::intrusive_ptr<listen_impl_func<H_STREAM> > li_stream(
-                reinterpret_cast<listen_impl_func<H_STREAM>*>(impl.get()));
-            return SODIUM_MAKE_TUPLE(stream_(li_stream), n1);
-        }
     };  // end namespace impl
 };  // end namespace sodium
